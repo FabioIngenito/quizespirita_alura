4f156561729798b3a096844051dc5b978593f238
FabioIngenito <fabio.ingenito@hotmail.com>
2021-01-26 23:14:48 -0300

    Modificações da Aula 2

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
 import db from '../db.json';
-import Widget from '../src/components/Widget'
-import QuizLogo from '../src/components/QuizLogo'
-import QuizBackground from '../src/components/QuizBackground'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
+import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+
 
 // const Title = styled.h1`
 //   font-size: 50px;
@@ -38,8 +43,15 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+  //console.log('retorno do useState', name, setName)
+
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>Quiz Espírita - Modelo Base</title>
+      </Head>
       <QuizContainer>
         <QuizLogo />
         <Widget>
@@ -48,6 +60,28 @@ export default function Home() {
           </Widget.Header>
           <Widget.Content>
             <p>{db.description}</p>
+            <form onSubmit={function (infosDoEnvento) {
+                infosDoEnvento.preventDefault();
+
+                // router manda para a próxima página
+                router.push(`/quiz?name=${name}`);
+                //console.log('Fazendo uma submissão por meio do React');
+            }}
+            >
+              <input 
+              onChange={function(infosDoEnvento) {
+                  //console.log(infosDoEnvento.target.value);
+                  // State
+                  //name = infosDoEnvento.target.value;
+                  setName(infosDoEnvento.target.value);
+              }}
+              placeholder="Por favor, digite seu nome." 
+              />
+              <button type="submit" disabled={name.length === 0}>
+                  Jogar 
+                  {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
 
@@ -60,7 +94,7 @@ export default function Home() {
         </Widget>
         <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/omariosouto" />
+      <GitHubCorner projectUrl="https://github.com/FabioIngenito/quizespirita_alurabase" />
     </QuizBackground>
   );
 }
